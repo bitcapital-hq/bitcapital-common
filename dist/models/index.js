d2f212c33a2036a5c2b3b33922a8bdc6c1cf78fb
Luis Brito <luiseduardo14@gmail.com>
2019-02-02 10:53:07 -0200

    Build: distribution files for 1.1.0

--- dist/models/index.js
+++ dist/models/index.js
@@ -3,13 +3,13 @@ function __export(m) {
     for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
 }
 Object.defineProperty(exports, "__esModule", { value: true });
-__export(require("./Asset"));
 __export(require("./User"));
+__export(require("./Wallet"));
+__export(require("./Asset"));
+__export(require("./Card"));
 __export(require("./Consumer"));
 __export(require("./Domain"));
 __export(require("./OAuth"));
 __export(require("./Payment"));
 __export(require("./Transaction"));
-__export(require("./User"));
-__export(require("./Wallet"));
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi9saWIvbW9kZWxzL2luZGV4LnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7O0FBQUEsNkJBQXdCO0FBQ3hCLDRCQUF1QjtBQUN2QixnQ0FBMkI7QUFDM0IsOEJBQXlCO0FBQ3pCLDZCQUF3QjtBQUN4QiwrQkFBMEI7QUFDMUIsbUNBQThCO0FBQzlCLDRCQUF1QjtBQUN2Qiw4QkFBeUIiLCJzb3VyY2VzQ29udGVudCI6WyJleHBvcnQgKiBmcm9tIFwiLi9Bc3NldFwiO1xuZXhwb3J0ICogZnJvbSAnLi9Vc2VyJztcbmV4cG9ydCAqIGZyb20gXCIuL0NvbnN1bWVyXCI7XG5leHBvcnQgKiBmcm9tIFwiLi9Eb21haW5cIjtcbmV4cG9ydCAqIGZyb20gXCIuL09BdXRoXCI7XG5leHBvcnQgKiBmcm9tIFwiLi9QYXltZW50XCI7XG5leHBvcnQgKiBmcm9tIFwiLi9UcmFuc2FjdGlvblwiO1xuZXhwb3J0ICogZnJvbSBcIi4vVXNlclwiO1xuZXhwb3J0ICogZnJvbSBcIi4vV2FsbGV0XCI7XG4iXX0=
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi9saWIvbW9kZWxzL2luZGV4LnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7O0FBQUEsNEJBQXVCO0FBQ3ZCLDhCQUF5QjtBQUN6Qiw2QkFBd0I7QUFDeEIsNEJBQXVCO0FBQ3ZCLGdDQUEyQjtBQUMzQiw4QkFBeUI7QUFDekIsNkJBQXdCO0FBQ3hCLCtCQUEwQjtBQUMxQixtQ0FBOEIiLCJzb3VyY2VzQ29udGVudCI6WyJleHBvcnQgKiBmcm9tICcuL1VzZXInO1xuZXhwb3J0ICogZnJvbSBcIi4vV2FsbGV0XCI7XG5leHBvcnQgKiBmcm9tIFwiLi9Bc3NldFwiO1xuZXhwb3J0ICogZnJvbSBcIi4vQ2FyZFwiO1xuZXhwb3J0ICogZnJvbSBcIi4vQ29uc3VtZXJcIjtcbmV4cG9ydCAqIGZyb20gXCIuL0RvbWFpblwiO1xuZXhwb3J0ICogZnJvbSBcIi4vT0F1dGhcIjtcbmV4cG9ydCAqIGZyb20gXCIuL1BheW1lbnRcIjtcbmV4cG9ydCAqIGZyb20gXCIuL1RyYW5zYWN0aW9uXCI7XG4iXX0=
