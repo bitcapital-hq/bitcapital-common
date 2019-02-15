1a2a9c879aa2cfbac4e8f715f937e2c948c91ac0
Ricardo Victorio <ricardo.victorio@gmail.com>
2019-02-15 13:05:58 -0200

    Feat: expose CustodyPostbackFeature

--- dist/types/provider/features/index.d.ts
+++ dist/types/provider/features/index.d.ts
@@ -4,3 +4,4 @@ export { CustodyDepositFeature } from './CustodyDepositFeature';
 export { CustodyPaymentFeature } from './CustodyPaymentFeature';
 export { CustodyWithdrawFeature } from './CustodyWithdrawFeature';
 export { CustodyBoletoFeature } from './CustodyBoletoFeature';
+export { CustodyPostbackFeature } from './CustodyPostbackFeature';
--- lib/provider/features/index.ts
+++ lib/provider/features/index.ts
@@ -1,6 +1,7 @@
-export { CustodyAuditFeature } from './CustodyAuditFeature';
-export { CustodyBlockFeature } from './CustodyBlockFeature';
-export { CustodyDepositFeature } from './CustodyDepositFeature';
-export { CustodyPaymentFeature } from './CustodyPaymentFeature';
-export { CustodyWithdrawFeature } from './CustodyWithdrawFeature';
-export { CustodyBoletoFeature } from './CustodyBoletoFeature';
+export { CustodyAuditFeature } from "./CustodyAuditFeature";
+export { CustodyBlockFeature } from "./CustodyBlockFeature";
+export { CustodyDepositFeature } from "./CustodyDepositFeature";
+export { CustodyPaymentFeature } from "./CustodyPaymentFeature";
+export { CustodyWithdrawFeature } from "./CustodyWithdrawFeature";
+export { CustodyBoletoFeature } from "./CustodyBoletoFeature";
+export { CustodyPostbackFeature } from "./CustodyPostbackFeature";
