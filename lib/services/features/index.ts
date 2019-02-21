2373bc2624e10653cc83aff711550cd6336fd668
Ricardo Victorio <ricardo.victorio@gmail.com>
2019-02-21 12:23:09 -0300

    Feat: export/exposes CustodyCardWebService

--- lib/services/features/index.ts
+++ lib/services/features/index.ts
@@ -1,6 +1,6 @@
-export { default as CustodyAuditWebService } from './CustodyAuditWebService';
-export { default as CustodyBoletoWebService } from './CustodyBoletoWebService';
-export { default as CustodyDepositWebService } from './CustodyDepositWebService';
-export { default as CustodyPaymentWebService } from './CustodyPaymentWebService';
-export { default as CustodyWithdrawWebService } from './CustodyWithdrawWebService';
-
+export { default as CustodyAuditWebService } from "./CustodyAuditWebService";
+export { default as CustodyBoletoWebService } from "./CustodyBoletoWebService";
+export { default as CustodyCardWebService } from "./CustodyCardWebService";
+export { default as CustodyDepositWebService } from "./CustodyDepositWebService";
+export { default as CustodyPaymentWebService } from "./CustodyPaymentWebService";
+export { default as CustodyWithdrawWebService } from "./CustodyWithdrawWebService";
