5875637a38931c4bebea7d023855b4e3c670832e
Vitor Rangel <rangel.final@gmail.com>
2019-03-03 12:02:16 -0300

    Feat: Add mapper util

--- lib/utils/index.ts
+++ lib/utils/index.ts
@@ -1,3 +1,4 @@
+export * from "./Mapper";
 export { default as Observable, Observer } from "./Observable";
 export { default as RequestUtil, RequestSigningOptions, RequestSigningHeaders } from "./Request";
 export { default as Pagination, PaginatedArray, PaginationData, PaginationUtil } from "./Pagination";
