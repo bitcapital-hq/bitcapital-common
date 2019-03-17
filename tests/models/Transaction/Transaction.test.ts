6f3e704e0a18307b10c16b3aa10e46a75d20c0aa
Luis Brito <luiseduardo14@gmail.com>
2019-03-17 04:26:39 -0300

    Fix: Test fixes

--- tests/models/Transaction/Transaction.test.ts
+++ tests/models/Transaction/Transaction.test.ts
@@ -1,16 +1,14 @@
-import { Wallet, Payment, Transaction, TransactionType, TransactionSchema, User } from "../../../lib";
+import { Wallet, Payment, Transaction, TransactionType, TransactionSchema } from "../../../lib";
 import { TEST_PAYMENT } from "../Payment/Payment.test";
 import { TEST_WALLET } from "../Wallet/Wallet.test";
 import * as faker from "faker";
 import * as uuid from "uuid/v4";
-import { TEST_USER } from "../User/User.test";
 
 const TEST_TRANSACTION = (): TransactionSchema => ({
   id: uuid(),
   type: TransactionType.PAYMENT,
   source: TEST_WALLET(),
   payments: Array.from({ length: faker.random.number(5) }, () => TEST_PAYMENT()),
-  createdBy: TEST_USER(),
   additionalData: {
     asset_code: faker.finance.currencyCode(),
     asset_id: uuid(),
@@ -28,7 +26,6 @@ describe("lib.models.Transaction", () => {
     expect(transaction.type).toBe(schema.type);
     expect(transaction.source).toBeInstanceOf(Wallet);
     transaction.payments.forEach(payment => expect(payment).toBeInstanceOf(Payment));
-    expect(transaction.createdBy).toBeInstanceOf(User);
     expect(transaction.additionalData).toMatchObject(schema.additionalData);
 
     expect(await transaction.isValid()).toBe(true);
